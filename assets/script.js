a920838aaaf60a65ad8a1e0dd10ba1d245f247f9
GeoBeldi <117314102+GeoBeldi@users.noreply.github.com>
2022-11-14 17:42:24 +0000

    Introduction to JavaScript code

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,33 @@
-var topics = "HTML, CSS, Git, JavaScript";
-console.log(topics);
-
+/*var topics = "HTML, CSS, Git, JavaScript";
+console.log(topics);*/
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
-for (var x = 0; x < topics.length; x++) {
-  console.log(topics[x]);
-}
-/*if (topic === "HTML") {
-  console.log("Let's study HTML!");
-} else if (topic === "CSS") {
-  console.log("Let's study CSS!");
-} else if (topic === "Git") {
-  console.log("Let's study Git!");
-} else if (topic === "JavaScript") {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log("Please try again!");
-}
-*/
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-var shapes = ["triangle", "square", "pentagon", "circle"];
-console.log(shapes[0]);
+function listTopics() {
+  for (var x = 0; x < topics.length; x++) {
+    // x = x+1 MEANS x++;
+    console.log(topics[x]);
+  }
+}
+function selectTopic() {
+  if (randomTopic === "HTML") {
+    console.log("Let's study HTML!");
+  } else if (randomTopic === "CSS") {
+    console.log("Let's study CSS!");
+  } else if (randomTopic === "Git") {
+    console.log("Let's study Git!");
+  } else if (randomTopic === "JavaScript") {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log("Please try again!");
+  }
+}
+
+console.log("Here are the topics we learnt through Prework:");
+listTopics();
+
+console.log("Which topic should we study first?");
+selectTopic();
+
+/*var shapes = ["triangle", "square", "pentagon", "circle"];
+console.log(shapes[0]);*/
